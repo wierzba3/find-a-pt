6d856dd24acf487023a3b5bbc1be7fc61219f374
wierzba3 <jamesmw129@gmail.com>
2015-12-10 20:42:14 -0700

    add view for trainer profile

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
 	
-	url(r'^$', views.index, name='index'),
+	url(r'^$', views.index, name='index'), #change this to be 
+	url(r'^trainer_profiles/$', views.trainer_profiles, name='trainer_profiles'),
     #url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 	#url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 	#url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
--- main_app/views.py
+++ main_app/views.py
@@ -6,6 +6,9 @@ from trainer_app.models import TrainerProfile
 def index(request):
 	trainer_list = TrainerProfile.objects.all()
 	context = { 'trainer_list': trainer_list }
-	return render(request, 'main_app/index.html', context)
+	return render(request, "main_app/index.html", context)
 	#return render(request, 'main_app/index.html')
+	
+def trainer_profiles(request):
+	return render(request, "main_app/trainer_profiles.html");
 	
